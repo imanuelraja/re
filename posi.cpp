094df5d750d06b7ed618feba046c2fdfc5296a9a
imanuelraja <36298776+imanuelraja@users.noreply.github.com>
2018-02-20 17:20:05 +0530

    posi.cpp

---
+++ posi.cpp
@@ -0,0 +1,13 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+ int num;
+ cout<<"Enter the num"<<endl;
+ cin>>num;
+ if(num>0)
+ cout<<"Positive";
+ else
+ cout<<"Negative";
+ return 0;
+ }
