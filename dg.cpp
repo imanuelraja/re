6cdb855ec2560f3c76e7a14e5610edcf0c42b14b
Harini Ramasamy <36298776+imanuelraja@users.noreply.github.com>
2018-02-23 17:39:58 +0530

    Create dg.cpp

---
+++ dg.cpp
@@ -0,0 +1,16 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+int no,a=0;
+clrscr();
+cout<<"Enter any num : ";
+cin>>no;
+while(no>0)
+{
+no=no/10;
+a++;
+}
+cout<<"\n digits in given number is: "<<a;
+getch();
+}
