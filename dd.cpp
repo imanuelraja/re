8a6f6d1bc270069466aaff4ca4052a712276bcc2
Harini Ramasamy <36298776+imanuelraja@users.noreply.github.com>
2018-02-23 17:13:13 +0530

    Create dd.cpp

---
+++ dd.cpp
@@ -0,0 +1,25 @@
+#include<conio.h>
+#include<iostream.h>
+
+void main() 
+{
+clrscr();
+int a, b, c;
+cout <<"Enter any three numbers: ";
+cin>>a;
+cin>>b
+cin>>c;
+if(a>=b && a>=c) 
+{
+cout<<"Largest number: "<<a;
+}
+if(b>=a && b>=c) 
+{
+cout<<"Largest number: "<<b;
+}
+if(c>=a && c>=b) 
+{
+cout<<"Largest number: "<<c;
+}
+getch();
+}
