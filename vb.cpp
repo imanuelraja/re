b86940534fb30dc5dea97a6e3c63af724c213456
Harini Ramasamy <36298776+imanuelraja@users.noreply.github.com>
2018-02-23 17:37:23 +0530

    Create vb.cpp

---
+++ vb.cpp
@@ -0,0 +1,16 @@
+#include<iostream.h>
+using namespace std;
+int main()
+{
+    int i,n,sum=0;
+    cout<<"How many numbers? ";
+    cin>>n;
+ 
+    for(i=1;i<=n;++i)
+    {
+        sum+=i;
+    }
+    
+    cout<<"Sum="<<sum;
+    return 0;
+}
