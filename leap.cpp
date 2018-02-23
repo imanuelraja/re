5d7240306e9141eb462f567d66692484ce17ad62
Harini Ramasamy <36298776+imanuelraja@users.noreply.github.com>
2018-02-23 17:17:06 +0530

    Create leap.cpp

---
+++ leap.cpp
@@ -0,0 +1,12 @@
+#include<iostream.h>
+void main()
+{
+ int year;
+ cout<<"emter the year"<<endl;
+ cin>>year;
+ if(year%4==0)
+ cout<<"the year is leap;
+ else
+ cout<<"The year is not leap";
+ getch();
+ }
