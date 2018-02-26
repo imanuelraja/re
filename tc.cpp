acb9b2035fcf432b249c6205d1f6f418abb542f6
Harini Ramasamy <36298776+imanuelraja@users.noreply.github.com>
2018-02-26 18:00:00 +0530

    Create tc.cpp

---
+++ tc.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main ()
+
+{
+	int num;
+	cin >> num;
+	for (int i=0;i<=20;i++)
+	{int j=1;
+		for (;j<=10;j++)
+		{int z;
+		z=num*j;
+		cout << " " << z;}
+	cout << endl;
+	}
+	return 0;
+}
