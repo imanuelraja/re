6428dd25d05bec5e7ef2f906e7765b742fd3f112
Harini Ramasamy <36298776+imanuelraja@users.noreply.github.com>
2018-02-26 17:16:49 +0530

    Create evvvv.cpp

---
+++ evvvv.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+ int a;
+ int g;
+ 
+ 
+ cout<<"please enter num"<<endl;
+cin>>a;
+cin>>g;
+while(a<g){
+ 
+ if(a%2==0)
+ {
+ cout<<"even no. ="<<a<<endl;
+ 
+ }
+ a=a+1;
+}
+}
