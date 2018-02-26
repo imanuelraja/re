60740b0eba4ff475eda41b2451d0648bc559fdd6
Harini Ramasamy <36298776+imanuelraja@users.noreply.github.com>
2018-02-26 17:14:00 +0530

    Create oooo.cpp

---
+++ oooo.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+ 
+ int a;
+ int g;
+ cout<<" enter number "<<endl;
+ cin>>a;
+ cout<<" enter number "<<endl;
+ cin>>g;
+ for(;a<=g;)
+ {
+ cout<<a<<endl;
+ a=a+2;
+ }
+}
